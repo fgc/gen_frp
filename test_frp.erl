48bb1a35b000afc953545d3e9ef743e203f9e9a2
Juan Felipe García Catalán <jfgcatalan@gmail.com>
2011-11-18 10:22:03 +0100

    Testing dynamically created oneE

--- test_frp.erl
+++ test_frp.erl
@@ -8,7 +8,12 @@ init(_Args) ->
     gen_frp:formatE(gen_frp:oneE("Here we go")),
     Timer = gen_frp:timerE(1000),                                             %% every second
     gen_frp:formatE(Timer),                                                   %% print the timestamp
-    gen_frp:formatE(gen_frp:collectE(Timer, 0, fun(_X,Acc) -> Acc + 1 end)),  %% and the number of seconds elapsed
+    gen_frp:formatE(E = gen_frp:collectE(Timer, 0, fun(_X,Acc) -> Acc + 1 end)),  %% and the number of seconds elapsed
+
+    %%dynamic oneE test
+    gen_frp:collectE(E, 0, fun(_X, _Acc) ->
+				   gen_frp:formatE(gen_frp:oneE("another second")) end),
+
     {ok, []}.
 
 terminate(_Args, State) ->
